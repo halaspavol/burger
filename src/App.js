1a3e90287106b8f0ea905625cc5ffe10a10b8899
Pavol Halás <pavol.halas1@outlook.com>
2018-06-14 18:05:08 +0200

    getting back to working state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react'
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-class App extends Component {
+class App extends React.Component {
   render () {
     return (
         <Layout>
           <BurgerBuilder />
         </Layout>
-    );
+    )
   }
 }
 
-export default App;
+export default App
